75a8ade860d4489efd52e0e7fe432471dac686b8
RsdoSergio <rosadosergio25@gmail.com>
2026-04-01 11:51:22 +0200

    Se ha encapsulado mejor los atributos de la clase casilla (ya conoce color, posición en cuanto a fila columna, tipo y si esta ocupada)

--- src/tablero/casilla.h
+++ src/tablero/casilla.h
@@ -4,10 +4,13 @@ class Casilla {
 public:
 	enum TipoCasilla {
 		NORMAL,
-		POWER_LIGHT,   // casillas especiales de Root (plantas)
-		POWER_DARK     // casillas especiales de Rotten (zombies)
+		PODER
 	};
 
 	TipoCasilla tipo;
 	bool ocupada;
+	unsigned char r, g, b; // Color de la casilla
+	int fila, col;  // Posición en el tablero
+
+	void inicializa(int f, int c, TipoCasilla t, unsigned char cr, unsigned char cg, unsigned char cb);
 };
--- src/tablero/tablero.cpp
+++ src/tablero/tablero.cpp
@@ -2,16 +2,6 @@
 #include "freeglut.h"
 
 void Tablero::inicializaTablero() {
-	for (int i = 0; i < FILAS; i++) {
-		for (int j = 0; j < COLS; j++) {
-			casillas[i][j].ocupada = false;
-
-			casillas[i][j].tipo = Casilla::NORMAL;
-		}
-	}
-}
-
-void Tablero::dibujaTablero() {
 	// Patron del tablero Archon 9x9
 	// 0 = verde muy claro
 	// 1 = verde oscuro
@@ -33,13 +23,29 @@ void Tablero::dibujaTablero() {
 	unsigned char colores[3][3] = {
 		{220, 255, 220},  // 0: verde muy claro
 		{34,  100,  34},  // 1: verde oscuro
-		{ 85, 140,  40},  // 2: verde medio
+		{85, 140,  40},  // 2: verde medio
 	};
 
+	for (int i = 0; i < FILAS; i++) { // Inicialización del tipo de casilla y su color
+		for (int j = 0; j < COLS; j++) {
+			Casilla::TipoCasilla tipo;
+
+			if (((i + 1 == 1 || i + 1 == 9) && j + 1 == 5) || ((i + 1 == 1 || i + 1 == 5 || i + 1 == 9) && i + 1 == 5))
+				tipo = Casilla::PODER;
+			else
+				tipo = Casilla::NORMAL;
+
+			// Guardar color según patrón (dentro de casilla)
+			int p = patron[i][j];
+			casillas[i][j].inicializa(i, j, tipo, colores[p][0], colores[p][1], colores[p][2]);
+		}
+	}
+}
+
+void Tablero::dibujaTablero() {
 	for (int i = 0; i < FILAS; i++) {
 		for (int j = 0; j < COLS; j++) {
-			int tipo = patron[i][j];
-			glColor3ub(colores[tipo][0], colores[tipo][1], colores[tipo][2]);
+			glColor3ub(casillas[i][j].r, casillas[i][j].g, casillas[i][j].b);
 
 			float x = j * TAM_CELDA - (COLS * TAM_CELDA) / 2.0f;
 			float y = i * TAM_CELDA - (FILAS * TAM_CELDA) / 2.0f;
