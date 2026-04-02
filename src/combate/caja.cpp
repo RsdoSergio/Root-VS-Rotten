3f032db424ae4fe5781e2a6c48c030b6f0d3ff25
pparias <pablo.paz.arias@alumnos.upm.es>
2026-04-02 18:39:17 +0200

    primer intento de añadir las barras de vida y nombre de los personajes

--- src/combate/caja.cpp
+++ src/combate/caja.cpp
@@ -1,4 +1,6 @@
 #include "caja.h"
+#include "freeglut.h"
+#include <string>
 
 void caja::dibuja()
 {
@@ -6,4 +8,21 @@ void caja::dibuja()
 	techo.dibuja();
 	izquierda.dibuja();
 	derecha.dibuja();
+
+	dibujaHUD();
+}
+
+void caja::fDatos(const pieza& p1, const pieza& p2)
+{
+
+	vida1 = p1.getVida();
+	vidamax1 = p1.getVidamax();
+	vida2 = p2.getVida();
+	vidamax2 = p2.getVidamax();
+
+}
+
+void caja::dibujaHUD()
+{
+
 }
--- src/combate/caja.h
+++ src/combate/caja.h
@@ -1,9 +1,14 @@
 #pragma once
 #include "pared.h"
-const double ancho = 45; //calculo 9*2.8=25.2, para que las paredes queden justo en los bordes del tablero
-const double alto = 25.2;
+#include "pieza.h"
+
+const double ancho = 45; 
+const double alto = 25.2; //calculo 9*2.8=25.2, para que las paredes queden justo en los bordes del tablero
 const double a_2 = ancho / 2;
 const double h_2 = alto / 2;
+const double h_HUD = 6.0;
+
+
 class caja
 {
     pared suelo{ {-a_2, -h_2}, {a_2, -h_2}, {255, 255, 255} };
@@ -11,7 +16,15 @@ class caja
     pared izquierda{ {-a_2, -h_2}, {-a_2, h_2}, {255, 255, 255} };
     pared derecha{ {a_2, -h_2}, {a_2, h_2}, {255, 255, 255} };
 
+    double vida1, vidamax1, vida2, vidamax2;
+	std::string nombre1, nombre2;
+
+	void dibujaHUD();
+
 public:
+	caja() = default;
+
 	void dibuja();
+	void fDatos(const pieza& p1, const pieza& p2);
 };
 
