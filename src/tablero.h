341e9186d36dd5c33ed49c4320da41e50b01ee3e
Kintchu <galoquinchuela@gmail.com>
2026-05-26 12:21:59 +0200

    Se ha separado la logica del cursor del tablero

--- src/tablero.h
+++ src/tablero.h
@@ -9,8 +9,7 @@ constexpr float TAM_CELDA = 2.8f;
 class Tablero {
 public:
     void inicializaTablero();
-	void dibujaTablero(Pos cursor); //añadido argumento cursor para dibujar el borde del cursor en la casilla activa
-    //← solo esta, sin duplicados ni vector/pair
+	void dibujaTablero(); //← solo esta, sin duplicados ni vector/pair
     void colocarPiezasIniciales();
     void dibujaPiezas();
 private:
