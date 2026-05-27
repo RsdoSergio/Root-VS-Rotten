63745548808cb43e1eee93ed8e5b9e1d94ae7d1b
Emmanuel Molina Camacho <ed.molinacamacho@gmail.com>
2026-05-27 14:19:27 +0200

    Arreglo en probelam de comunicacion en cursor y posiciones que realmente recibía el tablero. Ahora el cursor es dibujado por el propio tablero y no por mundo

--- src/mundo.h
+++ src/mundo.h
@@ -9,10 +9,9 @@
 class Mundo
 {
 	Tablero tablero;
-	int turno = 0;
+	
 
-	Pos seleccionada;                    
-	Pos piezaSeleccionada;
+	
 	Cursor  cursor;                  // gestiona posicion y dibujo del cursor
 	int     turno = 0;
 	std::vector<Pos> casillasValidas;
